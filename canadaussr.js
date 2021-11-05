4677c9b2fa63b55fb36c8fcaad7d8e268948767e
Denis Kolomoyets <60981975+deniskolomoyets@users.noreply.github.com>
2021-11-05 18:17:05 +0300

    Update canadaussr.js

--- canadaussr.js
+++ canadaussr.js
@@ -9,20 +9,29 @@ const scores = [
     [6, 5],
 ];
 
+export default (scores) => {
+  let result = 0;
+  for (const score of scores) {
+    result += Math.sign(score[0] - score[1]);
+  }
+
+  if (result > 0) {
+    return 'canada';
+  }
+  if (result < 0) {
+    return 'ussr';
+  }
+
+  return null;
+};
 
 
 
-const func = scores.reduce( function(acc, value, index){
-   return [...acc, value[0] - value[1]];
-},[]);
-
-const end = func.
-
-
-
-console.log(numArray);
-
-
+// const func = scores.reduce( function(acc, value, index){
+//    return [...acc, value[0] - value[1]];
+// },[]);
+// const end = func.
+// console.log(numArray);
 // const result = reverseArr.reduce( function(acc, element){
 //     return acc - element
 //   });
